c1acd99d9536af149c3a59967ecb81702d02239c
Toin <antoni0scar@gmail.com>
2022-10-16 15:19:54 -0300

    Inclusão de estrutura básica de POO, para execução do projeto com caso de teste Nokia

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+System.Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero:"123456", modelo: "Modelo1", imei: "11111121222",memoria:64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Telegram");
